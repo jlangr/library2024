b091ff31afe702a8d1e4b451586267695e0d51db
Jeff L <jeff@langrsoft.com>
2024-10-30 18:27:56 -0600

    clean up classification service

--- library/src/main/java/com/loc/material/api/ClassificationService.java
+++ library/src/main/java/com/loc/material/api/ClassificationService.java
@@ -23,29 +23,27 @@ public class ClassificationService implements ClassificationApi {
    @Override
    public Material retrieveMaterial(String sourceId) {
       var materialJsonObject = isbnClient.retrieve(sourceId);
-      return createMaterial(sourceId, materialJsonObject);
+
+      var author = retrieveFirstAuthor(materialJsonObject);
+
+      return createMaterial(sourceId, materialJsonObject, author);
    }
 
-   private Material createMaterial(String sourceId, Map<String, Object> jsonObject) {
+   private Map<String, Object> retrieveFirstAuthor(Map<String, Object> jsonObject) {
+      var authors = (List<Map<String,Object>>) jsonObject.get("authors");
+      var firstAuthorKey = getString(authors.getFirst(), "key");
+      var authorClient = new AuthorClient(http);
+      return authorClient.retrieve(firstAuthorKey);
+   }
+
+   private Material createMaterial(String sourceId, Map<String, Object> materialJsonObject, Map<String, Object> authorJsonObject) {
       var material = new Material();
       material.setSourceId(sourceId);
       material.setFormat(MaterialType.BOOK);
-      material.setTitle(getString(jsonObject, "title"));
-      material.setYear(getString(jsonObject, "publish_date"));
-
-
-
-      var authors = (List<Map<String,Object>>) jsonObject.get("authors");
-      System.out.println("authors: " + authors);
-//      var firstAuthorName = getString(authors.getFirst(), "name");
-      var firstAuthorKey = getString(authors.getFirst(), "key");
-      var authorClient = new AuthorClient(http);
-      var author = authorClient.retrieve(firstAuthorKey);
-      System.out.println("author: " + author);
-
-      material.setAuthor(getString(author, "name"));
-
-      material.setClassification(getLibraryOfCongressClassification(jsonObject));
+      material.setTitle(getString(materialJsonObject, "title"));
+      material.setYear(getString(materialJsonObject, "publish_date"));
+      material.setAuthor(getString(authorJsonObject, "name"));
+      material.setClassification(getLibraryOfCongressClassification(materialJsonObject));
       return material;
    }
 
@@ -58,16 +56,6 @@ public class ClassificationService implements ClassificationApi {
    @SuppressWarnings("unchecked")
    private List<String> getList(Map<String, Object> response, String key) {
       return new ArrayList<>((List<String>) response.get(key));
-   }
-
-//   private String getFirstAuthorName(Map<String, Object> map) {
-//      var firstAuthor = getMap(getList(map, "authors"), 0);
-//      return (String) firstAuthor.get("name");
-//   }
-
-   @SuppressWarnings("unchecked")
-   private Map<String, Object> getMap(List<Object> list, int index) {
-      return (Map<String, Object>) list.get(index);
    }
 
    private String getString(Map<String, Object> map, String key) {
