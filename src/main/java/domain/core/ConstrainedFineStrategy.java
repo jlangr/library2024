5b8f357163c605f814939ad182a6451dc0beb24b
Jeff L <jeff@langrsoft.com>
2024-11-18 17:14:56 -0700

    add constants

--- src/main/java/domain/core/ConstrainedFineStrategy.java
+++ src/main/java/domain/core/ConstrainedFineStrategy.java
@@ -1,6 +1,9 @@
 package domain.core;
 
 public class ConstrainedFineStrategy implements LateStrategy{
+   public static final short BASE_FEE = 100;
+   public static final short MAX_FINE = 1000;
+
    private final int fineBasis;
 
    public ConstrainedFineStrategy(int fineBasis) {
